3b35e5d14180cfedd4c1905843d2719887472b19
ConnorLayson <clayson2573@gmail.com>
2026-09-20 10:15:17 -0400

    Made the functions do the right things and display products

--- product_catalog.py
+++ product_catalog.py
@@ -1,62 +1,54 @@
 from product_data import products
-# TODO: Step 1 - Print out the products to see the data that you are working with.
-
-
-
-# TODO: Step 2 - Create a list called customer_preferences and store the user preference in this list.
-
 
 response = ""
+requested_tags = []
 while response != "N":
+    print()
     print("Input a preference:")
     preference = input()
-    # Add the customer preference to the list
+    requested_tags.append(preference)
 
-    response = input("Do you want to add another preference? (Y/N): ").upper()
-  
+    response = input("Do you want to add another preference? (Y/N): ").upper()[0]
+    #adding [0] protects against people putting "yes" or "no"
 
-# TODO: Step 3 - Convert customer_preferences list to set to eliminate duplicates.
+requested_tags = set(requested_tags)
 
-
-
-# TODO: Step 4 - Convert the product tags to sets in order to allow for faster comparisons.
-converted_products = []
-
-
-
-
-# TODO: Step 5 - Write a function to calculate the number of matching tags
 def count_matches(product_tags, customer_tags):
-    '''
-    Args:
-        product_tags (set): A set of tags associated with a product.
-        customer_tags (set): A set of tags associated with the customer.
-    Returns:
-        int: The number of matching tags between the product and customer.
-    '''
-    pass
+    count = 0
+    for tag in product_tags:
+        if tag in customer_tags:
+            count += 1
+    return count
 
-
-
-
-# TODO: Step 6 - Write a function that loops over all products and returns a sorted list of matches
 def recommend_products(products, customer_tags):
-    '''
-    Args:
-        products (list): A list of product dictionaries.
-        customer_tags (set): A set of tags associated with the customer.
-    Returns:
-        list: A list of products containing product names and their match counts.
-    '''
-    pass
+    recomend = []
 
+    for product in products:
+        for tag in customer_tags:
+            if tag in product["tags"]:
+                recomend.append(
+                    {
+                        "name": product["name"],
+                        "matches": count_matches(product["tags"],customer_tags)
+                    }
+                )
+                break
+    return recomend
 
+def sortKey(e):
+    return e["matches"]
 
-# TODO: Step 7 - Call your function and print the results
-
-
+print()
+print("--==RESULTS==--")
+results = recommend_products(products,requested_tags)
+results.sort(key=sortKey, reverse=True)
+for result in results:
+    print(f' - {result["name"]} ({result["matches"]} match(es))')
 
 
 # DESIGN MEMO (write below in a comment):
 # 1. What core operations did you use (e.g., intersections, loops)? Why?
+#   I used a lot of for loops to iterate through lists easily
+#   I also used the key argument in the sort function to tell it to sort by the count, grouping them together
 # 2. How might this code change if you had 1000+ products?
+#   I would probably find a better way of iterating through the list of products to find tags
